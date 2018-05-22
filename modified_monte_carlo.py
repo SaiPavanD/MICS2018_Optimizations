9d969c2ade9639bbb9717258c38527eb7b1ae62f
Sai Pavan D <saipvn4@gmail.com>
2018-05-22 14:55:34 +0200

    bug-free and unoptimized

--- modified_monte_carlo.py
+++ modified_monte_carlo.py
@@ -3,6 +3,7 @@ import networkx as nx
 from math import ceil
 import metis
 from itertools import count, ifilterfalse
+from collections import Counter
 
 class modified_monte_carlo(monte_carlo):
     def __init__(self, data):
@@ -15,33 +16,67 @@ class modified_monte_carlo(monte_carlo):
         self._color_count = []
         self._colors = [0] * self._data_pointer.shape[0]
         self._idle_nodes = [0] * self._data_pointer.shape[0]
+        self._partition_counts = None
 
     def process(self):
         # divide the graph into partitions such that each partition contains around 50 nodes
         (self._edgecuts, self._partitions) = metis.part_graph(self._data_pointer_nx,nparts=int(ceil(self._data_pointer_nx.number_of_nodes()/50.0)))
         self._color_count = [1] * (max(self._partitions)+1)
         # run the monte carlo on each of the partitions
-        while(sum(self._colored_nodes) < self._data_pointer.shape[0]):
-            self._idle_nodes = [0] * self._data_pointer.shape[0]
-            for i in xrange(self._data_pointer.shape[0]):
-                if self._colored_nodes[i] == 1:
-                    self._idle_nodes[i] = 1
-            temp_mis = self._get_mis()
-            for i in temp_mis:
-                self._colors[i] = self._color_count[self._partitions[i]]
-                self._colored_nodes[i] = 1
-            self._color_count[self._partitions[i]] += 1
+        self._partition_counts = Counter(self._partitions)
+        for partition_num in xrange(max(self._partitions)+1):
+            while(sum([self._colored_nodes[idx] for idx in xrange(self._data_pointer.shape[0]) if self._partitions[idx] == partition_num]) < self._partition_counts[partition_num]):
+                # print sum([self._colors[idx] for idx in xrange(self._data_pointer.shape[0]) if self._partitions[idx] == partition_num]), self._partition_counts[partition_num]
+                self._idle_nodes = [0] * self._data_pointer.shape[0]
+                for i in xrange(self._data_pointer.shape[0]):
+                    if self._colored_nodes[i] == 1:
+                        self._idle_nodes[i] = 1
+                temp_mis = self._get_mis(partition_num)
+                # if temp_mis == []:
+                #     exit(0)
+                # print temp_mis, [idx for idx in xrange(self._data_pointer.shape[0]) if self._partitions[idx] == partition_num]
+                for i in temp_mis:
+                    self._colors[i] = self._color_count[self._partitions[i]]
+                    self._colored_nodes[i] = 1
+                self._color_count[self._partitions[i]] += 1
         # merge the colored partitions and resolve any coloring conflicts
-        # for node_index in xrange(self._data_pointer.shape[0]):
-        #     self._resolve_color_conflicts(node_index)
+        # print self.get_colors()
+        for node_index in xrange(self._data_pointer.shape[0]):
+            self._resolve_color_conflicts(node_index)
 
+    def _get_mis(self, partition_num):
+        mis = []
+        while (sum([self._idle_nodes[idx] for idx in xrange(self._data_pointer.shape[0]) if self._partitions[idx]==partition_num]) < self._partition_counts[partition_num]):
+            temp_is = self._get_is(partition_num)
+            for i in temp_is:
+                self._idle_nodes[i]=1
+                mis += [i]
+                for j in self._get_neighbors(i,partition_num):
+                    self._idle_nodes[j]=1
+        return mis
 
-    def _get_neighbors(self, node_index):
-        return [index for index in xrange(self._data_pointer.shape[0]) if self._data_pointer[node_index,index] != 0 and self._idle_nodes[index] == 0 and self._partitions[index] == self._partitions[node_index]]
+    def _get_is(self, partition_num):
+        ind_set = []
+        for node_idx in xrange(self._data_pointer.shape[0]):
+            if self._partitions[node_idx] == partition_num:
+                if (self._is_local_max(node_idx, partition_num)):
+                    ind_set += [node_idx]
+        return ind_set
+
+    def _is_local_max(self, node_idx, partition_num):
+        if self._idle_nodes[node_idx] == 1:
+            return False
+        for i in self._get_neighbors(node_idx, partition_num):
+            if(self._node_weights[i] >= self._node_weights[node_idx]):
+                return False
+        return True
+
+    def _get_neighbors(self, node_index, partition_num):
+        return [index for index in xrange(self._data_pointer.shape[0]) if self._data_pointer[node_index,index] != 0 and self._idle_nodes[index] == 0 and self._partitions[index] == partition_num ]
 
     def _resolve_color_conflicts(self, node_index):
         for index in xrange(self._data_pointer.shape[0]):
-            if self._data_pointer[node_index,index] != 0 and self._partitions[index] != self._partitions[node_index] and self._colors[index] == self._colors[node_index]:
+            if self._data_pointer[node_index,index] != 0 and self._colors[index] == self._colors[node_index]:
                 self._assign_min_color(index, node_index)
 
     def _assign_min_color(self, node1, node2):
@@ -54,7 +89,7 @@ class modified_monte_carlo(monte_carlo):
                 col_list2.add(self._colors[index])
         col1 = next(ifilterfalse(col_list1.__contains__, count(1)))
         col2 = next(ifilterfalse(col_list2.__contains__, count(1)))
-        print node1, node2, self._colors[node1], col1, col2
+        # print node1, node2, self._colors[node1], self._partitions[node1], self._partitions[node2]
         if col1 < col2:
             self._colors[node1] = col1
         else:
@@ -64,7 +99,7 @@ class modified_monte_carlo(monte_carlo):
         self._idle_nodes = [0] * self._data_pointer.shape[0]
         count = 0
         for i in xrange(self._data_pointer.shape[0]):
-            for j in self._get_neighbors(i):
+            for j in monte_carlo._get_neighbors(self,i):
                 if self._colors[i] == self._colors[j]:
                     print "Error at node - {} partition - {}, node - {} partition {} with color {}".format(i,self._partitions[i],j,self._partitions[j], self._colors[i])
                     count+=1
--- test.py
+++ test.py
@@ -11,7 +11,7 @@ g = None
 # for iter in xrange(5):
 #     print "Iteration number - {}".format(iter + 1)
 #     print ""
-for f in sorted(glob('./DIMACS_graphs/*125.1*.col.txt')):
+for f in sorted(glob('./DIMACS_graphs/*125*.col.txt')):
     with open(f,'r') as fp:
         num_nodes = int(fp.readline())
         graph =  lil_matrix((num_nodes, num_nodes))
@@ -23,27 +23,28 @@ for f in sorted(glob('./DIMACS_graphs/*125.1*.col.txt')):
             graph[n2, n1] = 1
             g.add_edge(n1,n2)
 
-    # dsat = dsatur(graph)
-    # start1 = timer()
-    # dsat.process()
-    # end1 = timer()
-    # print f.split('/')[-1]
-    # # print "Monte Carlo check - ",
-    # # dsat.check_colors()
-    # print "DSATUR - {} colors in {} sec".format(max(dsat.get_colors()),end1-start1)
-    #
-    # mc = monte_carlo(graph)
-    # start2 = timer()
-    # mc.process()
-    # end2 = timer()
-    # # print "Monte Carlo check - ",
-    # # mc.check_colors()
-    # print "Monte Carlo - {} colors in {} sec".format(max(mc.get_colors()),end2-start2)
+    dsat = dsatur(graph)
+    start1 = timer()
+    dsat.process()
+    end1 = timer()
+    print f.split('/')[-1]
+    # print "Monte Carlo check - ",
+    # dsat.check_colors()
+    print "DSATUR - {} colors in {} sec".format(max(dsat.get_colors()),end1-start1)
+
+    mc = monte_carlo(graph)
+    start2 = timer()
+    mc.process()
+    end2 = timer()
+    # print "Monte Carlo check - ",
+    # mc.check_colors()
+    print "Monte Carlo - {} colors in {} sec".format(max(mc.get_colors()),end2-start2)
 
     mmc = modified_monte_carlo(g)
     start3 = timer()
     mmc.process()
     end3 = timer()
+    print "Modified Monte Carlo check",
     mmc.check_colors()
     print "Modified Monte Carlo - {} colors in {} sec".format(max(mmc.get_colors()),end3-start3)
     print ""
